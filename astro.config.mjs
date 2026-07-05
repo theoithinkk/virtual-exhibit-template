801aac54f6f7917f41d342954d0a4a9d8d08e527
theoithinkk <154042191+theoithinkk@users.noreply.github.com>
2026-07-05 12:42:21 +0800

    deployment configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()],
+  integrations: [react(), mdx(), tailwind(), icon()],
   site: 'https://your-org.github.io',
   base: '/silicon-minds/',
 });
